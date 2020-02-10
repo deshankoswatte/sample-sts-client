79c3e733ccd3d4a44bd781beab2c3c2b250e0bdd
deshankoswatte <dehami.deshan@gmail.com>
2020-02-10 17:17:33 +0530

    Removed previous client.

--- src/Client.java
+++ src/Client.java
@@ -1,92 +1,48 @@
-import javax.xml.soap.MessageFactory;
-import javax.xml.soap.SOAPConnection;
-import javax.xml.soap.SOAPConnectionFactory;
-import javax.xml.soap.SOAPConstants;
+import exception.WSTrustClientException;
+import util.ClientUtils;
+
 import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.TimeZone;
 
-import constants.Constants;
-import constants.Requests;
+import java.io.IOException;
+
+import static constants.Constants.ACTION_RENEW;
+import static constants.Constants.ACTION_REQUEST;
+import static constants.Constants.ACTION_VALIDATE;
 
 public class Client {
-
     public static void main(String[] args) {
 
-        // Enable this property to debug ssl related problems
-//        System.setProperty("javax.net.debug", "ssl");
-        System.setProperty("javax.net.ssl.trustStore", Constants.TRUST_STORE_LOCATION);
-        System.setProperty("javax.net.ssl.trustStorePassword", Constants.TRUST_STORE_PASSWORD);
+        SOAPMessage request_ST_RSTR;
+        SOAPMessage renew_ST_RSTR;
+        SOAPMessage validate_ST_RSTR;
 
-        SOAPMessage request = null;
         try {
-            request = buildRequest();
-        } catch (IOException e) {
-            System.out.println("Error while creating the SOAP Message. The input " +
-                    "stream passed was unreadable: " + e.getMessage());
+            request_ST_RSTR = ClientUtils.callSoapWebService(ACTION_REQUEST);
+            System.out.println("Issued Security Token RSTR: ");
+            request_ST_RSTR.writeTo(System.out);
+            System.out.println("\n\n\n\n\n");
+
+            String uri = request_ST_RSTR.getSOAPBody().getElementsByTagName("wsse:Reference").item(0).getAttributes().getNamedItem("URI").getNodeValue();
+            uri = uri.substring(1);
+
+            renew_ST_RSTR = ClientUtils.callSoapWebService(ACTION_RENEW, uri);
+            System.out.println("Renewed Security Token RSTR: ");
+            renew_ST_RSTR.writeTo(System.out);
+            System.out.println("\n\n\n\n\n");
+
+            System.out.println("Validate Security Token RSTR: ");
+            validate_ST_RSTR = ClientUtils.callSoapWebService(ACTION_VALIDATE, uri);
+            validate_ST_RSTR.writeTo(System.out);
+
         } catch (SOAPException e) {
-            System.out.println("Error while creating the SOAP Message. Error in creating " +
-                    "the specified implementation of Message Factory: " + e.getMessage());
-        }
-
-        callSoapWebService(Constants.STS_ENDPOINT_URL, request);
-    }
-
-    private static void callSoapWebService(String soapEndpointUrl, SOAPMessage soapMessage) {
-
-        try {
-            // Create SOAP Connection
-            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
-            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
-
-            // Send SOAP Message to SOAP Server
-            SOAPMessage soapResponse = soapConnection.call(soapMessage, soapEndpointUrl);
-
-            // Print the SOAP Response
-            System.out.println("Response SOAP Message:");
-            soapResponse.writeTo(System.out);
-            System.out.println();
-
-            soapConnection.close();
-        } catch (Exception e) {
-            System.err.println("\nError occurred while sending SOAP Request to Server!\nMake sure you have the correct endpoint URL and SOAPAction!\n");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (WSTrustClientException e) {
             e.printStackTrace();
         }
-    }
 
-    private static SOAPMessage buildRequest() throws IOException, SOAPException {
 
-        String[] timeStamps = generateNewTimeStamps();
-
-        System.out.println(Requests.buildRSTToRequestSecurityToken(timeStamps[0], timeStamps[1]));
-        InputStream byteArrayInputStream = new ByteArrayInputStream(Requests
-                .buildRSTToRequestSecurityToken(timeStamps[0], timeStamps[1]).getBytes());
-        SOAPMessage request = MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL)
-                .createMessage(null, byteArrayInputStream);
-        byteArrayInputStream.close();
-
-        return request;
-    }
-
-    private static String[] generateNewTimeStamps() {
-
-        String[] timeStamps = new String[2];
-
-        TimeZone timeZone = TimeZone.getTimeZone("UTC");
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-        dateFormat.setTimeZone(timeZone);
-
-        Calendar calendar = Calendar.getInstance();
-        timeStamps[0] = dateFormat.format(calendar.getTime());
-        calendar.add(Calendar.MINUTE, 5);
-        timeStamps[1]= dateFormat.format(calendar.getTime());
-
-        return timeStamps;
     }
 }
